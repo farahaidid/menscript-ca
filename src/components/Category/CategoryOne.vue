93d9301329616a8b2d27b0f37cf0c26fd6b44146
Peyas  chandra das <peyas3500@gmail.com>
2020-04-02 18:41:34 +0600

    categorybtn

--- src/components/Category/CategoryOne.vue
+++ src/components/Category/CategoryOne.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
         <div class="image1">
+           <div class="cat-image-div">
           <img src="@/assets/img/cat1.png" alt="" class="cat-one-img">
-          <div class="text mt-2">
-            <p class="sub-text-1 mb-0">Finasteride 1mg - Minoxdil 5% - Shampoo - Conditioner</p>
-            <p class="sub-text-2">28 tabletten - 60ml - 200ml - 200ml	- €56.98</p>
+          <div class="text">
+            <div class="hover-replace">
+
+        <button class="btn btn-secondary custom-cat-btn cat-one-btn " type="button">
+       Start online consult
+        </button>
+        <div class="cat-text">
+        <p class="sub-text-1 mb-0">Finasteride 1mg - Minoxdil 5% - Shampoo - Conditioner</p>
+        <p class="sub-text-2">28 tabletten - 60ml - 200ml - 200ml	- €56.98</p>
+      </div>
+          </div>
           </div>
         </div>
+        </div>
+
   </div>
 </template>
